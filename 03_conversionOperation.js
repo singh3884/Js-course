8bc4e90742d4aaaa5395ac64b2037387e18a47e9
Avinash <avinashraj3884@gmail.com>
2024-02-07 23:07:39 +0530

    Operation of js

--- 03_conversionOperation.js
+++ 03_conversionOperation.js
@@ -23,5 +23,26 @@ let booleanIsLoggedIn = Boolean(isLoggedIn)
 
  let someNumber = 33;
  let NumberToString=String(someNumber);
- console.log(NumberToString);
- console.log(typeof NumberToString);
+//  console.log(NumberToString);
+//  console.log(typeof NumberToString);
+
+
+ //*************************Operation*********************** 
+ let value =3;
+
+ let NegativeValue = -value;
+//  console.log(NegativeValue);
+
+//  console.log(2**3)//2 ka power 3;
+let str1 = "Avinash";
+let str2= " Raj";
+let str3= str1 +str2;
+// console.log(str3);
+
+// console.log("1"+2);
+// console.log(1+"2");
+// console.log(1+3+"2");
+// console.log("1"+3+2);
+
+//  console.log(+true);
+n 
