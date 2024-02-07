a1bfea5b7dedbb10aa024aff9660f591027a9688
Avinash <avinashraj3884@gmail.com>
2024-02-08 00:30:35 +0530

    Datatype Summary

---
+++ 05_datatype_summery.js
@@ -0,0 +1,29 @@
+// There are two type of data type 
+// 1) primitives  (ya call by value hai)
+//7 types:{string,Null ,Boolean ,null,undefined,Symbol,BigInt}
+const score =100;
+const scoreValue = 100.3
+const  isLoggedIn = false;
+const outsideTemp = null;
+// let userEmail;
+// const Id = symbol('123')
+// const anotherId = symbol('123')
+// console.log(Id === anotherId);
+const bigNumber =12234456677889n;
+// console.log(bigNumber);
+
+
+
+// 2) Non primitivs
+
+//Array,Objects,Functions
+const heros =["Rama","Shiva","Maa Durga","Hanuman ji"];
+let myObj ={
+    name :"Avinash",
+    age:20,
+}
+const myFunction = function(){
+    console.log("Jai Shree Ram");
+}
+
+console.log(typeof Id);
