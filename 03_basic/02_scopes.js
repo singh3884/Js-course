8a6f022fbe1905334f9e218d60bd0f519514a507
Avinash <avinashraj3884@gmail.com>
2024-02-11 16:47:48 +0530

    Scopes in js

---
+++ 03_basic/02_scopes.js
@@ -0,0 +1,13 @@
+
+// var c=200
+let a=200;
+if(true)
+{
+    let a=10;
+    const b=30;
+    var c=20;
+    console.log("Inner:",a);
+}
+console.log(a)
+// console.log(b)
+// console.log(c)//20(we can't use var )
