19646b9557306b5d1ccb488a58bf05f3f61a7e3b
Avinash <avinashraj3884@gmail.com>
2024-02-13 23:53:13 +0530

    Control flow in javascript in 1 shot

---
+++ 03_basic/04_control_flow/one.js
@@ -0,0 +1,55 @@
+//if
+const  isuserloggedin = true;
+const temprature =41;
+
+// if(temprature<50)
+// {
+//     console.log("less then 50");
+// }
+// else{
+//     console.log("temprature is grater than 50");
+
+// }
+// console.log("Excuted");
+//< ,>,<=,>=,==,!=,===,!==
+
+const score = 200;
+// if(score>100)
+// {
+//      const power ="fly"
+//      console.log(`user power :${power}`);
+// }
+// console.log(`user power :${power}`);
+
+const balance =1000;
+// if(balance>500) console.log("test"),console.log("test2");
+// if(balance<500)
+// {
+// console.log("less than 500");
+// }
+// else if(balance <750)
+// {
+//     console.log("less than 750"); 
+// }
+// else if(balance <900)
+// {
+//     console.log("less than 900"); 
+// }
+// else 
+// {
+//     console.log("less than 1200");
+// }
+const userloggedin = true;
+const debitCard = true;
+const loggedInFromGoogle =false
+const loggedInFromEmail =true
+
+if(userloggedin && debitCard && 2 == 3)
+{
+    console.log("Allow to buy course");
+}
+if(loggedInFromEmail ||  loggedInFromGoogle)
+{
+    console.log("user logged in");
+
+}
---
+++ 03_basic/04_control_flow/truthy.js
@@ -0,0 +1,56 @@
+const userEmail ="a@gmail.com";
+// if(userEmail)
+// {
+//     console.log("Got user Email");
+// }
+// else 
+// {
+//     console.log("Dont't have user Email");
+// }
+
+//falsy values
+
+//false,0,-0,BigInt 0n,"",null,undefined,NaN
+
+
+//truthy value
+//true,"0",'false'," ",[],{},function(){}
+// if(userEmail.length === 0) {
+//     console.log("Array is Empty")
+// }
+
+// const emptyObj= {}
+
+// if(Object.keys(emptyObj).length === 0)
+// {
+//     console.log("Object is Empty")
+// }
+
+
+
+//Nullish coalescing Operator(??):null undefined
+let val1;
+// val1 = 5 ?? 10
+// console.log(val1)//5
+
+
+// val1= null ?? 10;
+// console.log(val1)//10
+
+// var1 = undefined??15
+// console.log(val1)//undefined
+// val1= null ?? 10??70
+// console.log(val1)//10
+
+//terniary operator
+
+// condition ?  true:false;
+
+const iceTeaPrice =100;
+iceTeaPrice>=88 ? console.log("less then 88"):console.log("more than 80");
+
+
+
+
+
+
