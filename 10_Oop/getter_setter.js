2f8b7dc01839a43c97f7a9ff05893a87bbb3e896
Avinash <avinashraj3884@gmail.com>
2024-02-22 22:43:53 +0530

    Javascript ends with a story

---
+++ 10_Oop/getter_setter.js
@@ -0,0 +1,21 @@
+class User {
+    constructor(email, password){
+        this.email = email;
+        this.password = password
+    }
+    get email(){
+        return this._email.toUpperCase()
+    }
+    set email(value){
+        this._email = value
+    }
+    get password(){
+        return `${this._password}hitesh`
+    }
+    set password(value){
+        this._password = value;
+    }
+}
+    const hitesh = new User("h@hitesh.ai", "abc")
+    console.log(hitesh._password);
+    console.log(hitesh.email);
